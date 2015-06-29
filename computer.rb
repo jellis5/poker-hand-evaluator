6aae11975dacbae48e1f9c95cb8ff565d37ca4b4
Jacob Ellis <jellis5@users.noreply.github.com>
2015-06-29 03:44:47 -0500

    Create computer.rb

---
+++ computer.rb
@@ -0,0 +1,15 @@
+require_relative 'player'
+
+class Computer < Player
+	@@name_num = 1
+	
+	def initialize(chips_amount)
+		super("Computer_#{@@name_num}", chips_amount)
+		@@name_num += 1
+	end
+	
+	def take_turn
+		puts "#{@name}'s turn..."
+		sleep(2)
+	end
+end
