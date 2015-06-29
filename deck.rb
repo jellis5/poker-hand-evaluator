b43bb6cd01ea7a89fe542f4133eeef7fc646c5df
Jacob Ellis <jellis5@users.noreply.github.com>
2015-06-29 03:43:51 -0500

    Create deck.rb

---
+++ deck.rb
@@ -0,0 +1,24 @@
+require_relative 'card'
+
+class Deck
+	def initialize
+		@cards = []
+		(0..3).each do |suit|
+			(1..13).each do |rank|
+				@cards << Card.new(suit, rank)
+			end
+		end
+	end
+	
+	def shuffle!
+		@cards.shuffle!
+	end
+	
+	def draw_cards(num_cards)
+		num_cards.times.inject([]) { |acc, card| acc << @cards.pop }
+	end
+	
+	def to_s
+		@cards.inject([]) { |acc, card| acc << card.to_s }.join("\n")
+	end
+end
