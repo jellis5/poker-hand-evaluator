349de821deeabd93cda025618f11ea794125e1f0
Jacob Ellis <jellis5@users.noreply.github.com>
2015-07-03 05:15:01 -0500

    Change suit and rank class variables to constants

--- card.rb
+++ card.rb
@@ -1,14 +1,14 @@
 class Card
 	include Comparable
 	
-	@@suits = [:clubs, :diamonds, :hearts, :spades]
-	@@ranks = [nil, nil, :'2', :'3', :'4', :'5', :'6', :'7', :'8', :'9', :'10', :Jack, :Queen, :King, :Ace]
+	SUITS = [:clubs, :diamonds, :hearts, :spades]
+	RANKS = [nil, nil, :'2', :'3', :'4', :'5', :'6', :'7', :'8', :'9', :'10', :Jack, :Queen, :King, :Ace]
 	
 	attr_reader :rank_num, :suit, :rank
 	
 	def initialize(suit=0, rank=2)
 		@rank_num = rank
-		@suit, @rank = @@suits[suit], @@ranks[rank]
+		@suit, @rank = SUITS[suit], RANKS[rank]
 	end
 	
 	def <=>(other)
