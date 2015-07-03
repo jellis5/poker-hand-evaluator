b1ebde810be7c4f69897ef0d678208fcbe8e0a65
Jacob Ellis <jellis5@users.noreply.github.com>
2015-07-03 05:18:06 -0500

    Start turn processing and betting implementation

--- human.rb
+++ human.rb
@@ -5,21 +5,43 @@ class Human < Player
 		super(name, chips_amount)
 	end
 	
-	def take_turn
-		puts "Your turn.\n\n"
+	# implement option to see other person's chips later
+	def take_turn(pot)
+		choice = 1
+		puts "\nYour turn.\nChips: #{@chips}\n\n"
 		puts @hand
 		puts "\n1: Check"
 		puts "2: Bet"
 		puts "3: Fold"
 		loop do
 			begin
-				puts "Enter choice: "
+				print "Enter choice: "
 				choice = gets.chomp.to_i
 				raise TypeError if choice < 1 || choice > 3
 				break
 			rescue TypeError
 				retry
 			end
+		end
+		case choice
+		when 1
+			puts "#{@name} checks. Press enter to continue."
+			gets
+			return [1]
+		when 2
+			amount = 0
+			loop do
+				begin
+					print "Enter bet amount: "
+					amount = gets.chomp.to_i
+					raise TypeError if amount < 1 || amount > @chips
+					break
+				rescue TypeError
+					retry
+				end
+			end
+			@chips -= amount
+			return [2, amount]
 		end
 	end
 end
