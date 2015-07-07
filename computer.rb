c1da50161be3f4228a9fcbb1af9e2fafbcd9b4d7
Jacob Ellis <jellis5@users.noreply.github.com>
2015-07-07 01:29:37 -0500

    Started support for betting

--- computer.rb
+++ computer.rb
@@ -8,9 +8,15 @@ class Computer < Player
 		@@name_num += 1
 	end
 	
-	def take_turn
+	def take_turn(current_bet)
 		puts "#{@name}'s turn..."
-		puts "#{@name} checks."
+		if current_bet > 0
+			@chips -= current_bet - @current_bet_amount
+			@current_bet_amount += current_bet
+			puts "#{@name} calls. Chips: #{@chips}"
+		else
+			puts "#{@name} checks."
+		end
 		return [1]
 		sleep(0)
 	end
