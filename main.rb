b1333832cd01ceb7a02314eafc484c6a47fb2666
Jacob Ellis <jellis5@users.noreply.github.com>
2015-07-07 01:30:13 -0500

    Started support for betting

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ require_relative 'card' # take this out later
 # players array needs to cycle through players starting at a certain index
 class Array
 	def each_from_start(start_index)
-		index, iters = start_index, 0
+		index, iters = start_index >= self.length ? start_index % self.length : start_index, 0
 		if block_given?
 			while iters < self.length
 				iters += 1
@@ -84,7 +84,7 @@ def main
 			raise TypeError if num_comps < 1 || num_comps > 5
 			break
 		rescue TypeError
-			retry
+			redo
 		end
 	end
 	players = init_players(you, num_comps, chip_amount)
@@ -93,24 +93,31 @@ def main
 	while players.map{ |player| player.chips > 0 }.length > 1
 		deck = Deck.new
 		deck.shuffle!
+		active_players = players.dup
 		com_cards = []
 		pot = 0
 		puts "\nHand number: #{hand_num}"
 		puts "Dealer: #{players[dealer_index]}\n"
 		deal_hands(deck, players)
 		loop do
-			#take turns (bet, check, fold or call, raise, fold)
-			#add com card
-			first_turn = dealer_index + 1
-			first_turn = first_turn % num_players if first_turn >= num_players
+			first_turn = dealer_index + 1 >= active_players.length ? 0 : dealer_index + 1
 			print_com_cards(com_cards)
 			puts "Pot: #{pot}\n\n"
-			players.each_from_start(first_turn) do |player|
-				turn_arr = player.take_turn
+			current_bet = 0
+			last_turn_index = first_turn - 1 < 0 ? active_players.length - 1 : first_turn - 1
+			loop do
+				puts first_turn, last_turn_index
+				turn_arr = active_players[first_turn].take_turn(current_bet)
 				if turn_arr[0] == 2
-					pot += turn_arr[1]
+					current_bet += turn_arr[1]
+					last_turn_index = first_turn - 1 < 0 ? active_players.length - 1 : first_turn - 1
+				elsif turn_arr[0] == 3
+					active_players.delete_at(first_turn)
 				end
+				break if (first_turn == last_turn_index) && (active_players[first_turn].current_bet_amount == current_bet)
+				first_turn = first_turn + 1 >= active_players.length ? 0 : first_turn + 1
 			end
+			active_players.each { |player| player.current_bet_amount = 0 }
 			break if com_cards.length == 5
 			# if pre-flop (length == 0), add flop; else add turn and then river
 			com_cards.push(*deck.draw_cards(com_cards.length == 0 ? 3 : 1))
